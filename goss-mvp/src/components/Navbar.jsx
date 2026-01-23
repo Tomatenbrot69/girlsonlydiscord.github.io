de36786846edc8f87d86727f95c7da67d6debbf7
NilAtabey <nilatabey@gmail.com>
2026-01-23 10:07:56 +0100

    case sensitive asset path fixed

--- goss-mvp/src/components/Navbar.jsx
+++ goss-mvp/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import GOSSTag from "../assets/GOSStag.png";
+import GOSSTag from "../assets/GOSSTag.png";
 
 export default function Navbar() {
   return (
